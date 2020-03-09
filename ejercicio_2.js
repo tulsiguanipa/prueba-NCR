a0de4c274aaa24f95a4109e5edb98d48938636be
Tulsi Guanipa <tulsiguanipa@gmail.com>
2020-03-09 17:11:33 -0300

    act

--- ejercicio_2.js
+++ ejercicio_2.js
@@ -1,8 +1,27 @@
-let cadena = "hola, que buena ola Laomir";
-let indices = [];
-
-for(let i = 0; i < cadena.length; i++) {
-	if (cadena[i].toLowerCase() == "a") indices.push(i);
+function buscador(texto, palabra){
+    
+    let x = 0, y=0;
+   
+    for (i=0;i< texto.length;i++)
+        {
+        if(texto[i] == palabra[0])
+            {
+            for(j=i;j< i+palabra.length;j++)
+               {
+                if(texto[j]==palabra[j-i])
+                  {
+                    y++;
+                  }
+                if (y==palabra.length){
+                    x++;
+                }
+            }
+            y=0;
+        }
+    }
+   return "'"+palabra+"' encontrada "+x+" veces.";
 }
 
-console.log(indices.length);
+console.log(buscador('hola, que buena ola Laomir', 'o' + 'l' + 'a'));
+console.log(buscador('hola, que buena ola Laomir', 'a' + 'o'));
+
